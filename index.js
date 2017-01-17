4aece8e270c640d3bd4f360cd8be4287b57a0f5e
Rich <ibanez19@sbcglobal.net>
2017-01-17 18:46:49 +0000

    Completed code on index.js

--- index.js
+++ index.js
@@ -2,10 +2,37 @@ var express = require('express'); //Import ExpressJS
 var app = express(); //Server
 var port = process.env.PORT || 8080; //Set port
 
-app.get('/', function(req, res) {
-   res.send('Hello World!');
-});
+//Parse all of the necessary data
+function parseIt(headers) {
+    //Create object to return at end of function
+    var d = headers;
+    console.log('d: ' + d);
+    
+    var parsedObj = {
+        'ipaddress': d['x-forwarded-for'], //IP Address directly from header
+        'language': '',
+        'software': ''
+    };
+    
+    //Parse langauge from correct header
+    var pLang = d['accept-language'];
+    pLang = pLang.slice(0, pLang.indexOf(','));
+    parsedObj.language = pLang; //Assign to object
+    
+    //Parse software from correct header
+    var pSoft = d['user-agent'];
+    pSoft = pSoft.slice(pSoft.indexOf('(')+1,pSoft.indexOf(')'));
+    parsedObj.software = pSoft; //Assign to object
+    
+    //Return complete object
+    return parsedObj;
+}
 
+
+app.get('/', function(req, res) {
+    //Parse data and display to visitor
+   res.send(parseIt(req.headers));
+});
 
 
 app.listen(port, function() {
