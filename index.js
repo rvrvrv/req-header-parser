5357f389c62c74c481241b0a4459c84b2f7501fd
Rich <ibanez19@sbcglobal.net>
2018-09-26 16:46:00 +0000

    Use ES6

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-var express = require('express'); //Import ExpressJS
-var app = express(); //Server
-var port = process.env.PORT || 8080; //Set port
+const express = require('express'); // Import ExpressJS
+const app = express(); // Server
+const port = process.env.PORT || 8080; // Set port
 
-//Parse all of the necessary data
+// Parse all of the necessary data
 function parseIt(headers) {
-    //Create object to return at end of function
-    var d = headers;
+  // Create object to return at end of function
+  const h = headers;
 
-    var parsedObj = {
-        'ipaddress': d['x-forwarded-for'], //IP address directly from header
-        'language': '',
-        'software': ''
-    };
-    
-    //Parse langauge from correct header
-    var pLang = d['accept-language'];
-    pLang = pLang.slice(0, pLang.indexOf(','));
-    parsedObj.language = pLang; //Assign to object
-    
-    //Parse software from correct header
-    var pSoft = d['user-agent'];
-    pSoft = pSoft.slice(pSoft.indexOf('(')+1,pSoft.indexOf(')'));
-    parsedObj.software = pSoft; //Assign to object
-    
-    //Return complete object
-    return parsedObj;
+  const parsedObj = {
+    ipAddress: h['x-forwarded-for'], // IP address directly from header
+    language: '',
+    software: '',
+  };
+
+  // Parse language from correct header
+  let pLang = h['accept-language'];
+  pLang = pLang.slice(0, pLang.indexOf(','));
+  parsedObj.language = pLang; // Assign to object
+
+  // Parse software from correct header
+  let pSoft = h['user-agent'];
+  pSoft = pSoft.slice(pSoft.indexOf('(') + 1, pSoft.indexOf(')'));
+  parsedObj.software = pSoft; // Assign to object
+
+  // Return complete object
+  return parsedObj;
 }
 
-
-app.get('/', function(req, res) {
-    //Parse data and display to visitor
-   res.send(parseIt(req.headers));
+app.get('/', (req, res) => {
+  // Parse data and display to visitor
+  res.send(parseIt(req.headers));
 });
 
-
-app.listen(port, function() {
-    console.log('Server is listening on port ' + port);
-})
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
